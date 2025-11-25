c982b9544f77e58f658be8806219682bd7608b2d
eueduardoaugusto <eueduardoaugustosr@gmail.com>
2025-11-24 23:22:56 -0300

    Update cantinho_pet_oficial.sql

--- back-end/cantinho_pet_oficial.sql
+++ back-end/cantinho_pet_oficial.sql
@@ -1,91 +1,286 @@
-CREATE DATABASE IF NOT EXISTS cantinho_pet;
-USE cantinho_pet;
+CREATE DATABASE  IF NOT EXISTS `cantinho_pet` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cantinho_pet`;
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cantinho_pet
+-- ------------------------------------------------------
+-- Server version	8.0.42
 
-CREATE TABLE users (
-  id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  senha VARCHAR(255) NOT NULL,
-  cargo ENUM('proprietario', 'funcionario', 'admin') DEFAULT 'funcionario',
-  data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE clientes (
-  id_cliente INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(100) NOT NULL,
-  cpf VARCHAR(14) UNIQUE,
-  telefone VARCHAR(20),
-  email VARCHAR(100),
-  cep VARCHAR(10),
-  logadouro VARCHAR(20),
-  endereco VARCHAR(200),
-  numero VARCHAR(10),
-  complemento VARCHAR(20),
-  cidade VARCHAR(60),
-  estado CHAR(2),
-  data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+--
+-- Table structure for table `clientes`
+--
 
-CREATE TABLE pets (
-  id_pet INT AUTO_INCREMENT PRIMARY KEY,
-  id_cliente INT,
-  nome_pet VARCHAR(50) NOT NULL,
-  especie ENUM('Cachorro', 'Gato') NOT NULL,
-  raca VARCHAR(50),
-  idade INT,
-  observacao TEXT,
-  FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
-);
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id_cliente` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `cpf` varchar(255) DEFAULT NULL,
+  `cep` varchar(255) DEFAULT NULL,
+  `logadouro` varchar(255) DEFAULT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `numero` varchar(255) DEFAULT NULL,
+  `complemento` varchar(255) DEFAULT NULL,
+  `cidade` varchar(255) DEFAULT NULL,
+  `estado` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `telefone` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE fornecedores (
-  id_fornecedor INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(100) NOT NULL,
-  cnpj VARCHAR(18) UNIQUE,
-  telefone VARCHAR(20),
-  email VARCHAR(100),
-  endereco VARCHAR(200)
-);
+--
+-- Table structure for table `fornecedores`
+--
 
-CREATE TABLE produtos (
-  id_produto INT AUTO_INCREMENT PRIMARY KEY,
-  codigo_ean CHAR(13) UNIQUE,
-  nome VARCHAR(100) NOT NULL,
-  tamanho VARCHAR(20),
-  setor ENUM('Caes','Gatos'),
-  grupo ENUM('Servico','Brinquedo', 'Racao', 'Petisco', 'Acessorio', 'Higiene', 'Farmacia', 'Outros') NOT NULL,
-  preco DECIMAL(10,2) NOT NULL,
-  quantidade_estoque INT DEFAULT 0,
-  id_fornecedor INT,
-  FOREIGN KEY (id_fornecedor) REFERENCES fornecedores(id_fornecedor)
-);
+DROP TABLE IF EXISTS `fornecedores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fornecedores` (
+  `id_fornecedor` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `cnpj` varchar(18) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `endereco` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_fornecedor`),
+  UNIQUE KEY `cnpj` (`cnpj`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE agendamentos (
-  id_agendamento INT AUTO_INCREMENT PRIMARY KEY,
-  id_cliente INT NOT NULL,
-  id_pet INT NOT NULL,
-  id_servico INT NOT NULL,
-  data_horario DATETIME NOT NULL,
-  status ENUM('Pendente', 'Confirmado', 'Concluido', 'Cancelado') DEFAULT 'Pendente',
-  FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
-  FOREIGN KEY (id_pet) REFERENCES pets(id_pet),
-  FOREIGN KEY (id_servico) REFERENCES produtos(id_produto)
-);
+--
+-- Table structure for table `grupos`
+--
 
-CREATE TABLE vendas (
-  id_venda INT AUTO_INCREMENT PRIMARY KEY,
-  id_cliente INT,
-  data_venda TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  valor_total DECIMAL(10,2),
-  tipo_pagamento ENUM('Dinheiro', 'Cartao', 'Pix', 'Outros'),
-  FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
-);
+DROP TABLE IF EXISTS `grupos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `grupos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `setor` varchar(255) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE itens_venda (
-  id_item INT AUTO_INCREMENT PRIMARY KEY,
-  id_venda INT,
-  id_produto INT,
-  quantidade INT DEFAULT 1,
-  preco_unitario DECIMAL(10,2),
-  FOREIGN KEY (id_venda) REFERENCES vendas(id_venda),
-  FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
-);
+--
+-- Table structure for table `itens_venda`
+--
+
+DROP TABLE IF EXISTS `itens_venda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `itens_venda` (
+  `itemSold_id` int NOT NULL AUTO_INCREMENT,
+  `id_venda` int DEFAULT NULL,
+  `id_produto` int DEFAULT NULL,
+  `quantidade` int DEFAULT NULL,
+  `preco_unitario` decimal(10,2) DEFAULT NULL,
+  `desconto` decimal(10,2) DEFAULT NULL,
+  `preco_final` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`itemSold_id`),
+  KEY `id_venda` (`id_venda`),
+  KEY `id_produto` (`id_produto`),
+  CONSTRAINT `itens_venda_ibfk_1` FOREIGN KEY (`id_venda`) REFERENCES `vendas` (`id_venda`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `itens_venda_ibfk_2` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id_produto`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `notas_fiscais`
+--
+
+DROP TABLE IF EXISTS `notas_fiscais`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notas_fiscais` (
+  `id_nota_fiscal` int NOT NULL AUTO_INCREMENT,
+  `rps` varchar(255) DEFAULT NULL,
+  `nfs_e` varchar(255) DEFAULT NULL,
+  `data_emissao` datetime DEFAULT NULL,
+  `id_venda` int DEFAULT NULL,
+  PRIMARY KEY (`id_nota_fiscal`),
+  KEY `id_venda` (`id_venda`),
+  CONSTRAINT `notas_fiscais_ibfk_1` FOREIGN KEY (`id_venda`) REFERENCES `vendas` (`id_venda`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `orcamentos`
+--
+
+DROP TABLE IF EXISTS `orcamentos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orcamentos` (
+  `id_orcamento` int NOT NULL AUTO_INCREMENT,
+  `orcamento` varchar(255) DEFAULT NULL,
+  `nome_cliente` varchar(255) DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `data_emissao` datetime DEFAULT NULL,
+  `id_cliente` int DEFAULT NULL,
+  `id_usuario` int DEFAULT NULL,
+  PRIMARY KEY (`id_orcamento`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `orcamentos_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `orcamentos_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `parcelas_pagamento`
+--
+
+DROP TABLE IF EXISTS `parcelas_pagamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `parcelas_pagamento` (
+  `paymentParcel_id` int NOT NULL AUTO_INCREMENT,
+  `forma_pagamento` varchar(255) DEFAULT NULL,
+  `numero_parcela` int DEFAULT NULL,
+  `valor_parcela` decimal(10,2) DEFAULT NULL,
+  `data_vencimento` datetime DEFAULT NULL,
+  `id_venda` int DEFAULT NULL,
+  PRIMARY KEY (`paymentParcel_id`),
+  KEY `id_venda` (`id_venda`),
+  CONSTRAINT `parcelas_pagamento_ibfk_1` FOREIGN KEY (`id_venda`) REFERENCES `vendas` (`id_venda`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `pets`
+--
+
+DROP TABLE IF EXISTS `pets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pets` (
+  `id_pet` int NOT NULL AUTO_INCREMENT,
+  `id_cliente` int DEFAULT NULL,
+  `nome_pet` varchar(50) NOT NULL,
+  `especie` enum('Cachorro','Gato') NOT NULL,
+  `raca` varchar(50) DEFAULT NULL,
+  `idade` int DEFAULT NULL,
+  `observacao` text,
+  PRIMARY KEY (`id_pet`),
+  KEY `id_cliente` (`id_cliente`),
+  CONSTRAINT `pets_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produtos` (
+  `id_produto` int NOT NULL AUTO_INCREMENT,
+  `codigo_ean` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `tamanho` varchar(255) NOT NULL,
+  `setor` varchar(255) DEFAULT NULL,
+  `grupo` varchar(255) DEFAULT NULL,
+  `id_fornecedor` int NOT NULL,
+  `quantidade_estoque` int DEFAULT NULL,
+  `preco` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id_produto`),
+  KEY `id_fornecedor` (`id_fornecedor`),
+  CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`id_fornecedor`) REFERENCES `fornecedores` (`id_fornecedor`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `setor`
+--
+
+DROP TABLE IF EXISTS `setor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `setor` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `sigla` varchar(255) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `cargo` enum('proprietario','funcionario','admin') DEFAULT 'funcionario',
+  `data_criacao` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `vendas`
+--
+
+DROP TABLE IF EXISTS `vendas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vendas` (
+  `id_venda` int NOT NULL AUTO_INCREMENT,
+  `id_orcamento` int DEFAULT NULL,
+  `id_cliente` int DEFAULT NULL,
+  `id_usuario` int DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `data_emissao` datetime DEFAULT NULL,
+  `tipo` varchar(255) DEFAULT NULL,
+  `qt_parcelas` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id_venda`),
+  KEY `id_orcamento` (`id_orcamento`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `vendas_ibfk_1` FOREIGN KEY (`id_orcamento`) REFERENCES `orcamentos` (`id_orcamento`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `vendas_ibfk_2` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `vendas_ibfk_3` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-24 21:02:13
